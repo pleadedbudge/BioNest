31033c86a60f1d542acf18da66a4feb0193b3b14
pleadedbudge <teotiaseema05@gmail.com>
2025-02-08 11:46:16 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,12 @@
 export default function Home() {
   return (
-    <div style={{ textAlign: "center", padding: "50px", color: "#fff", background: "linear-gradient(to right, black, red)" }}>
+    <div style={{ 
+      textAlign: "center", 
+      padding: "50px", 
+      color: "#fff", 
+      minHeight: "100vh",
+      background: "linear-gradient(to right, black, red)"
+    }}>
       <h1>Welcome to BioNest</h1>
       <p>A free and advanced web app like Discord!</p>
     </div>
